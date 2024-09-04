06ffc93bb99267578c6336aaf657e166484d9a7d
krisawyerr <Krisawyerrr@gmail.com>
2024-09-04 10:55:11 -0700

    Added json data and started integration

---
+++ SipLog/DataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  DataLoader.swift
+//  SipLog
+//
+//  Created by Kris Sawyerr on 9/4/24.
+//
+
+import Foundation
---
+++ SipLog/DrinksModel.swift
@@ -0,0 +1,8 @@
+//
+//  DrinksModel.swift
+//  SipLog
+//
+//  Created by Kris Sawyerr on 9/4/24.
+//
+
+import Foundation

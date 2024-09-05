7340e1fbf0424588a80494c8f56f79ce7fef4942
krisawyerr <Krisawyerrr@gmail.com>
2024-09-04 20:21:28 -0700

    Finished all drinks page

---
+++ SipLog/View/AllDrinksListView.swift
@@ -0,0 +1,18 @@
+//
+//  AllDrinksListView.swift
+//  SipLog
+//
+//  Created by Kris Sawyerr on 9/4/24.
+//
+
+import SwiftUI
+
+struct AllDrinksListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllDrinksListView()
+}
